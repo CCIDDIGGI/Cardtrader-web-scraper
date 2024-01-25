cb5affba902d1a3ed931e1640337d4f35d258ba4
CCIDDIGGI <marco.boccolacci@gmail.com>
2024-01-25 15:14:16 +0100

    first implementation of the winForm

---
+++ Cardtrader web scraper/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Cardtrader_web_scraper.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cardtrader_web_scraper.Controllers
+{
+    public class UserController: Controller
+    {
+        private List<User> users = new List<User>();
+
+        // Constructor
+        public UserController()
+        {
+            
+        }
+
+        #region User Actions
+
+        /// <summary>
+        /// Adds a new user to the current users list
+        /// </summary>
+        /// <param name="user">The user to add to the list</param>
+        public void addUser(User user)
+        {
+            users.Add(user);   
+        }
+
+        #endregion
+    }
+}
